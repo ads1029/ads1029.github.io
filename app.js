2dfe68d5726e743d8316b8aaa8ef36f80889dc8e
Minjie Huang <millerk1029@gmail.com>
2020-11-29 03:40:28 +1100

    The new start

---
+++ app.js
@@ -0,0 +1,19 @@
+// alert('Hello World') //! Better not do this!
+
+'use strict';
+console.log('here is new start');
+
+let today = new Date();
+
+let formatdate = today.toDateString();
+let selectElement = document.getElementById('date');
+
+def(){
+    let a = 1;
+    console.log('checkcing indents in this shiiiiiiit');
+}
+
+
+
+selectElement.innerHTML = formatdate;
+
