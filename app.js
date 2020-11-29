5aaa258e2156b94507c6a7d065a8332e0ff8dd7b
Minjie Huang <millerk1029@gmail.com>
2020-11-29 23:40:04 +1100

    finish nav button

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 // alert('Hello World') //! Better not do this!
 
-'use strict';
-console.log('here is new start');
+// 'use strict';
+console.log('app.js is loaded.');
 
-let today = new Date();
+// let today = new Date();
 
-let formatdate = today.toDateString();
-let selectElement = document.getElementById('date');
+// let formatdate = today.toDateString();
+// let selectElement = document.getElementById('date');
 
-def(){
-    let a = 1;
-    console.log('checkcing indents in this shiiiiiiit');
-}
+// def(){
+//     let a = 1;
+//     console.log('checkcing indents in this shiiiiiiit');
+// }
 
 
 
-selectElement.innerHTML = formatdate;
+// selectElement.innerHTML = formatdate;
 
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks= document.querySelectorAll('.nav__link');
+
+
+navToggle.addEventListener('click', () => {
+    // console.log('trying red');
+    document.body.classList.toggle('nav-open')
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click',() => {
+        document.body.classList.remove('nav-open');
+    })
+})
